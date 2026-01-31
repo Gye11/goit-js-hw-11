e99da08924c3b363e15b7d18a13085d8344d664b
Gye11 <hilallornekk@gmail.com>
2026-01-31 03:26:11 +0300

    final

---
+++ src/main.js
@@ -0,0 +1,103 @@
+import axios from 'axios';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.search-form');
+const gallery = document.querySelector('.gallery');
+const loader = document.querySelector('.loader');
+
+const API_KEY = 'BURAYA_PIXABAY_API_KEY_YAZ';
+const BASE_URL = 'https://pixabay.com/api/';
+
+const lightbox = new SimpleLightbox('.gallery a');
+
+form.addEventListener('submit', onSearch);
+
+function onSearch(event) {
+  event.preventDefault();
+
+  const query = event.target.elements.query.value.trim();
+
+  if (!query) {
+    iziToast.warning({
+      message: 'Please enter a search term!',
+    });
+    return;
+  }
+
+  clearGallery();
+  showLoader();
+
+  fetchImages(query)
+    .then(images => {
+      if (images.length === 0) {
+        iziToast.error({
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+        });
+        return;
+      }
+
+      renderGallery(images);
+      lightbox.refresh();
+    })
+    .catch(() => {
+      iziToast.error({
+        message: 'Something went wrong. Please try again later.',
+      });
+    })
+    .finally(() => {
+      hideLoader();
+    });
+}
+
+async function fetchImages(query) {
+  const response = await axios.get(BASE_URL, {
+    params: {
+      key: API_KEY,
+      q: query,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+    },
+  });
+
+  return response.data.hits;
+}
+
+function renderGallery(images) {
+  const markup = images
+    .map(
+      img => `
+      <li class="gallery-item">
+        <a href="${img.largeImageURL}">
+          <img src="${img.webformatURL}" alt="${img.tags}" />
+        </a>
+        <div class="info">
+          <p>❤️ ${img.likes}</p>
+          <p>👁 ${img.views}</p>
+          <p>💬 ${img.comments}</p>
+          <p>⬇️ ${img.downloads}</p>
+        </div>
+      </li>
+    `
+    )
+    .join('');
+
+  gallery.insertAdjacentHTML('beforeend', markup);
+}
+
+function clearGallery() {
+  gallery.innerHTML = '';
+}
+
+function showLoader() {
+  loader.classList.remove('is-hidden');
+}
+
+function hideLoader() {
+  loader.classList.add('is-hidden');
+}
